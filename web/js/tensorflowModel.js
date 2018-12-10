d172c1fb438dd49831114c9e6a624bde95de57b9
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-12-10 20:34:46 +0100

    cleaned up a lot, also fixed a loading bug in html

--- web/js/tensorflowModel.js
+++ web/js/tensorflowModel.js
@@ -1,9 +1,15 @@
-console.log('write tf, model in console');
+console.log('write tf/model in console');
 let model;
-const modelPromise = tf.loadModel('http://localhost:8080/web/models/TF.js/faceEmotionWeights/model.json');
+const MODEL_PATH = 'http://localhost:8080/web/models/TF.js/faceEmotionWeights/model.json';
+let IMAGE_SIZE = 48;
 
+let statusElement = document.getElementById("status");
+const status = msg => (statusElement.innerText = msg);
 
-modelPromise.then(function(value) {
-  console.log(value); // Success!
-  model = value;
-});
+const MODEL_PROMISE = async () => {
+  status("Loading model...");
+  model = await tf.loadModel(MODEL_PATH);
+  status("Model loaded");
+};
+
+MODEL_PROMISE();
