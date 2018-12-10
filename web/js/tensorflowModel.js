b820b294f1683bd1126c88f6f7a93a0df7bcde7d
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-12-10 20:42:27 +0100

    took over guillaumes methods

--- web/js/tensorflowModel.js
+++ web/js/tensorflowModel.js
@@ -3,6 +3,7 @@ let model;
 const MODEL_PATH = 'http://localhost:8080/web/models/TF.js/faceEmotionWeights/model.json';
 let IMAGE_SIZE = 48;
 
+let predictionsElement = document.getElementById("predictions");
 let statusElement = document.getElementById("status");
 const status = msg => (statusElement.innerText = msg);
 
@@ -11,5 +12,75 @@ const MODEL_PROMISE = async () => {
   model = await tf.loadModel(MODEL_PATH);
   status("Model loaded");
 };
+
+async function predict(imgElement) {
+  status("Predicting...");
+  const startTime = performance.now();
+  const logits = tf.tidy(() => {
+    // tf.fromPixels() returns a Tensor from an image element.
+    const img = tf.fromPixels(imgElement).toFloat();
+    let gray_img = img.mean(2);
+    let final_img = gray_img.expandDims(2);
+
+    // Is it needed ?
+    // const offset = tf.scalar(127.5);
+    // // Normalize the image from [0, 255] to [-1, 1].
+    // const normalized = final_img.sub(offset).div(offset);
+
+    // Reshape to a single-element batch so we can pass it to predict.
+    const batched = final_img.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 1]);
+    // Make a prediction through mobilenet.
+    return model.predict(batched);
+  });
+  // Shoe inference time
+  const totalTime = performance.now() - startTime;
+  status(`Done in ${Math.floor(totalTime)}ms`);
+  // Show the classes in the DOM.
+  showResults(imgElement, logits);
+}
+
+function showResults(imgElement, logits) {
+  const predictionContainer = document.createElement("div");
+  predictionContainer.className = "pred-container";
+  //
+  const probsContainer = document.createElement("div");
+  probsContainer.innerText = "class: " + logits.as1D().argMax().dataSync();
+  predictionContainer.appendChild(probsContainer);
+  //
+  const imgContainer = document.createElement("div");
+  imgContainer.appendChild(imgElement);
+  predictionContainer.appendChild(imgContainer);
+  //
+  predictionsElement.insertBefore(
+    predictionContainer,
+    predictionsElement.firstChild
+  );
+}
+
+const filesElement = document.getElementById("files");
+filesElement.addEventListener("change", evt => {
+  let files = evt.target.files;
+  // Display thumbnails & issue call to predict each image.
+  for (let i = 0, f; (f = files[i]); i++) {
+    // Only process image files (skip non image files)
+    if (!f.type.match("image.*")) {
+      continue;
+    }
+    let reader = new FileReader();
+    const idx = i;
+    // Closure to capture the file information.
+    reader.onload = e => {
+      // Fill the image & call predict.
+      let img = document.createElement("img");
+      img.src = e.target.result;
+      img.width = IMAGE_SIZE;
+      img.height = IMAGE_SIZE;
+      img.onload = () => predict(img);
+    };
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+  }
+});
+
 
 MODEL_PROMISE();
