1f918c64f899c68571d6ac1f9c335616f9c7156b
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-12-12 22:46:33 +0100

    added cut out picture

--- web/js/takeScreenshot.js
+++ web/js/takeScreenshot.js
@@ -1,8 +1,11 @@
 let canvas = document.querySelector('canvas');
+let canvas2 = document.getElementById('abc');
+let ctx = canvas2.getContext('2d');
 let context = canvas.getContext('2d');
 let width, height, ratio;
 let timeout = 10; //milliseconds
 
+let clip;
 
 video.onloadedmetadata = function() {
   //Calculate the ratio
@@ -16,8 +19,21 @@ video.onloadedmetadata = function() {
 
 
 function snap() {
+  clip = context.getImageData(video.width/2-50, video.height/2-50, video.width/2-50, video.height/2);
+  // let l = clip.data.length / 4;
+  //
+  // for (let i = 0; i < l; i++) {
+  //   let r = clip.data[i * 4 + 0];
+  //   let g = clip.data[i * 4 + 1];
+  //   let b = clip.data[i * 4 + 2];
+  //   if (g > 100 && r > 100 && b < 43){
+  //     clip.data[i * 4 + 3] = 0;
+  //   }
+  // }
+  ctx.putImageData(clip, 0, 0);
+
   // Define the size of the rectangle that will be filled (basically the entire element)
-  context.fillRect(0, 0, width, height);
+  // context.fillRect(0, 0, width, height);
   // Grab the image from the video
   context.drawImage(video, 0, 0, width, height);
   // Call the function again
