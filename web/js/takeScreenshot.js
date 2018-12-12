7ffe7bb03a225bbfbad8a082a7d7cd784e4899e0
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-12-12 18:50:29 +0100

    added automatic  snapshoting of camera

--- web/js/takeScreenshot.js
+++ web/js/takeScreenshot.js
@@ -1,6 +1,7 @@
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 let width, height, ratio;
+let timeout = 10; //milliseconds
 
 
 video.onloadedmetadata = function() {
@@ -8,17 +9,22 @@ video.onloadedmetadata = function() {
   ratio = video.width / video.height;
   width = video.width;
   height = video.height;
-  // Set the canvas width and height to the values just calculated
+  // Set the canvas width and height
   canvas.width = width;
   canvas.height = height;
 }
 
-// Takes a snapshot of the video
+
 function snap() {
   // Define the size of the rectangle that will be filled (basically the entire element)
   context.fillRect(0, 0, width, height);
   // Grab the image from the video
   context.drawImage(video, 0, 0, width, height);
+  // Call the function again
+  setTimeout(function() {
+    snap();
+  }, timeout);
 }
+snap();
 
 //based on: https://html5multimedia.com/code/ch9/video-canvas-screenshot.html
