16d330f6829cfc00089883f15cc78ebf5783922a
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-12-19 16:11:37 +0100

    cleanup startGame.js

--- web/js/startGame.js
+++ web/js/startGame.js
@@ -1,35 +1,30 @@
 const overlay = document.getElementById("overlay");
 const camOverlay = document.getElementById("camOverlay");
 let text = document.getElementById("ready").getElementsByTagName("h1")[0];
-
-
 overlay.addEventListener("click", startGame);
 
 activateOverlay();
 
-
 function activateOverlay() {
   overlay.style.display = "block";
-  // camOverlay.style.display = "none";
 }
 
 function startGame() {
   //Prevent multiclicks
   overlay.removeEventListener("click", startGame);
-
+  //count 3-2-1
   for (let i = 0; i < 3; i++) {
     setTimeout(function() {
       text.innerHTML = 3 - i;
     }, i * 1000);
   }
+  //write go
   setTimeout(function() {
     text.innerHTML = "go";
   }, 3000);
+  //start game
   setTimeout(function() {
-    // camOverlay.style.display = "block";
     overlay.style.display = "none";
-    //starts the game
     getforms();
   }, 4000);
-
 }
