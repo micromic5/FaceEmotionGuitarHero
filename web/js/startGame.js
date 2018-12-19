9abb37da9464d9328f9c78cbab5603d6803eaa4e
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-12-19 12:59:33 +0100

    added counter in overlay, starts the game

--- web/js/startGame.js
+++ web/js/startGame.js
@@ -1,5 +1,7 @@
 const overlay = document.getElementById("overlay");
 const camOverlay = document.getElementById("camOverlay");
+let text = document.getElementById("ready").getElementsByTagName("h1")[0];
+
 
 overlay.addEventListener("click", startGame);
 
@@ -12,6 +14,21 @@ function activateOverlay() {
 }
 
 function startGame() {
-  overlay.style.display = "none";
-  camOverlay.style.display = "block";
+  //Prevent multiclicks
+  overlay.removeEventListener("click", startGame);
+  for (let i = 0; i < 3; i++) {
+    setTimeout(function() {
+      text.innerHTML = 3 - i;
+    }, i * 1000);
+  }
+  setTimeout(function() {
+    text.innerHTML = "go";
+  }, 3000);
+  setTimeout(function() {
+    overlay.style.display = "none";
+    camOverlay.style.display = "block";
+    //starts the game
+    getforms();
+  }, 4000);
+
 }
