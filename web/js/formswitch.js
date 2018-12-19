cfc09df2279379f6bb9c687822208ecffa6f6206
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-12-19 10:29:45 +0100

    fixed randomFunction

--- web/js/formswitch.js
+++ web/js/formswitch.js
@@ -1,36 +1,38 @@
-
-
 let mysvg = document.getElementsByTagName("svg");
-let mycircle = mysvg[0].cloneNode(true);
-let myhexa = mysvg[1].cloneNode(true);
-let mytriangle = mysvg[2].cloneNode(true);
-let mycube = mysvg[3].cloneNode(true);
-let myline = mysvg[4].cloneNode(true);
+const mycircle = mysvg[0].cloneNode(true);
+const myhexa = mysvg[1].cloneNode(true);
+const mytriangle = mysvg[2].cloneNode(true);
+const mycube = mysvg[3].cloneNode(true);
+const myline = mysvg[4].cloneNode(true);
 
 let previous = document.getElementById("previous");
 let current = document.getElementById("current");
 let next = document.getElementById("next");
 
-let randomForms = mysvg[Math.floor(Math.random()* mysvg.length)].cloneNode(true);
+let randomForms = mysvg[Math.floor(Math.random() * mysvg.length)].cloneNode(true);
 
-function getforms() {
-    //let mycircle = document.getElementsByTagName("svg")[0];
-    /*previous.appendChild(mycircle);
-    current.appendChild(mytriangle);
-    next.appendChild(myhexa);*/
 
-    //try random
-    previous.appendChild(randomForms);
-    current.appendChild(randomForms);
-    next.appendChild(randomForms);
-}
 getforms();
 
 
+function getRNDNumberOfSVGArray() {
+	return Math.floor(Math.random()* mysvg.length);
+}
+
+function cloneRNDNodeOfSVG() {
+	return mysvg[getRNDNumberOfSVGArray()].cloneNode(true);
+}
+
+function getforms() {
+  previous.appendChild(cloneRNDNodeOfSVG());
+  current.appendChild(cloneRNDNodeOfSVG());
+  next.appendChild(cloneRNDNodeOfSVG());
+}
 
 let previousStyle = previous.children[0].style;
 let currentStyle = current.children[0].style;
 let nextStyle = next.children[0].style;
+
 
 function styles() {
   previousStyle.transform = "scale(3,3)";
